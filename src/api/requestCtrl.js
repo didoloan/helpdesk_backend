561d71d517418d2e44ea7e6c7b1cb87b701b7747
Doyin Awosanya <didoloan@gmail.com>
2020-08-25 12:39:51 +0100

    added some request code

--- src/api/requestCtrl.js
+++ src/api/requestCtrl.js
@@ -1,17 +1,41 @@
 import requestDbOps from '../db/requests';
+import Coder from './coder';
 import { ObjectId } from 'mongodb';
+
+
 
 export default class requestCtrl{
     static async getWithHistory(req, res) {
-        let response = requestDbOps.getWithHistory()
+        if(!req.get('Authorization')){
+            res.json({error:'Not Authorized!'});
+            return;
+        }
+
+        let token = req.get('Authorization').split(" ")[1];
+
+        let userfromtoken = await Coder.jwtDecode(token);
+
+        let response = requestDbOps.getWithHistory(req.body.id);
+
         if(response.error){
             res.json({error: 'Problem establishing connection with database.'});
             return;
         }
+
         res.json(response);
     }
 
     static async add(req, res) {
+        if(!req.get('Authorization')){
+            res.json({error:'Not authorized'});
+            return;
+        }
+        let token = req.get('Authorization').split(" ")[1];
+        let userfromtoken = await Coder.jwtDecode(token);
+        if(userfromtoken.role==='technician'){
+            res.json({error:'Not authorized'});
+            return;
+        }
         let response = await requestDbOps.add({title:req.body.title, request_type:req.body.type, date_opened: new Date(), target_dept:req.body.target_dept, description:req.body.description, status:'open', comments:req.body.comment});
         if(response.insertedCount!=1){
             res.json({error:'DB error!'});
@@ -21,6 +45,16 @@ export default class requestCtrl{
     }
 
     static async update(req, res) {
+        if(!req.get('Authorization')){
+            res.json({error:'Not authorized'});
+            return;
+        }
+        let token = req.get('Authorization').split(" ")[1];
+        let userfromtoken = await Coder.jwtDecode(token);
+        if(userfromtoken.role!=='technician'){
+            res.json({error:'Not authorized'});
+            return;
+        }
         let response = await requestDbOps.update({user_id:ObjectId(req.body.user_id), feedback: req.body.feedback, time_taken: req.body.time_taken, comment: req.body.comment});
         if(response.insertedCount!=1){
             res.json({error:'DB error'});
